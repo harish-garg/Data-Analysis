b37d613a102290f8e96239978b5e66817d5d3012
Harish Garg <harish.garg@gmail.com>
2016-09-28 21:14:27 +0530

    Checking in some exercises for diamonds dataset analysis problem set

---
+++ R/Udacity_Intro_To_R/exploreDiamonds.R
@@ -0,0 +1,121 @@
+# Load the necessary libraries
+library(ggplot2)
+
+# Load the diamond dataset
+data(diamonds)
+
+# look at the data
+names(diamonds)
+summary(diamonds)
+diamonds$color
+
+# create plots
+# # Create a histogram of the price of
+# all the diamonds in the diamond data set.
+# =======================================
+qplot(data = diamonds, x=price)
+summary(diamonds$price)
+
+# How many diamonds cost less then $500?
+table(diamonds$price < 500)
+
+# How many diamonds cost less then $250?
+table(diamonds$price < 250)
+
+# How many diamonds cost eual to more then $15000?
+table(diamonds$price >= 15000)
+
+# Explore the largest peak in the
+# price histogram you created earlier.
+
+# Try limiting the x-axis, altering the bin width,
+# and setting different breaks on the x-axis.
+
+# There won’t be a solution video for this
+# question so go to the discussions to
+# share your thoughts and discover
+# what other people find.
+
+# You can save images by using the ggsave() command.
+# ggsave() will save the last plot created.
+# For example...
+#                  qplot(x = price, data = diamonds)
+#                  ggsave('priceHistogram.png')
+
+# ggsave currently recognises the extensions eps/ps, tex (pictex),
+# pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
+
+# Submit your final code when you are ready.
+
+# TYPE YOUR CODE BELOW THE LINE
+# ======================================================================
+qplot(x = price, data = diamonds, binwidth=100) +
+  coord_cartesian(xlim = c(0,2000))
+
+# Break out the histogram of diamond prices by cut.
+
+# You should have five histograms in separate
+# panels on your resulting plot.
+
+# TYPE YOUR CODE BELOW THE LINE
+# ======================================================
+qplot(data = diamonds, x = price) +
+  facet_wrap(~cut)
+
+# Diamond prices by cut
+by(diamonds$price, diamonds$cut, max)
+by(diamonds$price, diamonds$cut, min)
+by(diamonds$price, diamonds$cut, median)
+
+# In the two last exercises, we looked at
+# the distribution for diamonds by cut.
+
+# Run the code below in R Studio to generate
+# the histogram as a reminder.
+
+# ===============================================================
+
+qplot(x = price, data = diamonds) + facet_wrap(~cut)
+
+# ===============================================================
+
+# In the last exercise, we looked at the summary statistics
+# for diamond price by cut. If we look at the output table, the
+# the median and quartiles are reasonably close to each other.
+
+# diamonds$cut: Fair
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     337    2050    3282    4359    5206   18570 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Good
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     327    1145    3050    3929    5028   18790 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Very Good
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     336     912    2648    3982    5373   18820 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Premium
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     326    1046    3185    4584    6296   18820 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Ideal
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     326     878    1810    3458    4678   18810 
+
+# This means the distributions should be somewhat similar,
+# but the histograms we created don't show that.
+
+# The 'Fair' and 'Good' diamonds appear to have 
+# different distributions compared to the better
+# cut diamonds. They seem somewhat uniform
+# on the left with long tails on the right.
+
+# Let's look in to this more.
+
+# Look up the documentation for facet_wrap in R Studio.
+# Then, scroll back up and add a parameter to facet_wrap so that
+# the y-axis in the histograms is not fixed. You want the y-axis to
+# be different for each histogram.
+
+# If you want a hint, check out the Instructor Notes.
